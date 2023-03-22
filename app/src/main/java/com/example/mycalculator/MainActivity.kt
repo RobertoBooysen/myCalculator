47c566258c88ad49047bab7501f60df5c70e3457
RobertoBooysen <ST10085125@vcconnect.edu.za>
2023-03-22 14:42:58 +0200

    Third commit

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -15,13 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Declaring variables
+        //Declaring variables(The IIE,2023)
         var editTextNumberOne=findViewById<EditText>(R.id.editTextNumberOne)
         var editTextNumberTwo=findViewById<EditText>(R.id.editTextNumberTwo)
         var textViewDisplay=findViewById<TextView>(R.id.textViewDisplay)
         var textViewErrorDisplay=findViewById<TextView>(R.id.textViewErrorDisplay)
 
-        //Declaring buttons
+        //Declaring buttons(The IIE,2023)
         val btnPlus=findViewById<Button>(R.id.btnPlus)
         val btnMinus=findViewById<Button>(R.id.btnMinus)
         val btnMultiply=findViewById<Button>(R.id.btnMultiply)
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         val btnSquareRoot=findViewById<Button>(R.id.btnSquareRoot)
         val btnStatisticFunctions=findViewById<Button>(R.id.btnStatisticFunctions)
 
-        //Plus button
+        //Plus button(The IIE,2023)
         btnPlus?.setOnClickListener {
             Toast.makeText(this@MainActivity, "Plus button clicked", Toast.LENGTH_LONG).show()
 
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
 
             textViewDisplay.text ="Addition:  ${editTextNumberOne.text} + ${editTextNumberTwo.text} = $totalPlus "
         }
-        //Minus button
+        //Minus button(The IIE,2023)
         btnMinus?.setOnClickListener {
             Toast.makeText(this@MainActivity, "Minus button clicked", Toast.LENGTH_LONG).show()
 
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity() {
 
             textViewDisplay.text ="Subtraction:  ${editTextNumberOne.text} - ${editTextNumberTwo.text} = $totalMinus "
         }
-        //Multiply button
+        //Multiply button(The IIE,2023)
         btnMultiply?.setOnClickListener {
             Toast.makeText(this@MainActivity, "Multiply button clicked", Toast.LENGTH_LONG).show()
 
@@ -66,7 +66,7 @@ class MainActivity : AppCompatActivity() {
 
             textViewDisplay.text ="Multiplication:  ${editTextNumberOne.text} * ${editTextNumberTwo.text} = $totalMultiply "
         }
-        //Division button
+        //Division button(The IIE,2023)
         btnDivide?.setOnClickListener {
             Toast.makeText(this@MainActivity, "Divide button clicked", Toast.LENGTH_LONG).show()
 
@@ -83,7 +83,7 @@ class MainActivity : AppCompatActivity() {
                 textViewDisplay.text ="Division:  ${editTextNumberOne.text} / ${editTextNumberTwo.text} = $totalDivide "
             }
         }
-        //Power button
+        //Power button(The IIE,2023)
         btnPower?.setOnClickListener {
             Toast.makeText(this@MainActivity, "Power button clicked", Toast.LENGTH_LONG).show()
 
@@ -95,7 +95,7 @@ class MainActivity : AppCompatActivity() {
 
             textViewDisplay.text ="Power: ${editTextNumberOne.text} to the power of ${editTextNumberTwo.text}= $totalPower "
         }
-        //Square root button
+        //Square root button(The IIE,2023)
         btnSquareRoot?.setOnClickListener {
             Toast.makeText(this@MainActivity, "Square root button clicked", Toast.LENGTH_LONG).show()
 
@@ -105,14 +105,20 @@ class MainActivity : AppCompatActivity() {
 
             textViewDisplay.text ="Square root: √${editTextNumberOne.text} = $totalSquareRoot "
         }
-        //Statistic functions button
+        //Statistic functions button(The IIE,2023)
         btnStatisticFunctions?.setOnClickListener {
             Toast.makeText(this@MainActivity, "Statistic functions button clicked", Toast.LENGTH_LONG).show()
 
-            //Redirect to statistic functions page
+            //Redirect to statistic functions page(Schwarz, 2021)
             val intent= Intent(this,StatisticFunctions::class.java)
 
             startActivity(intent);
         }
     }
 }
+//References List:
+//Techie Delight .2016. Declare and initialize an array in Kotlin. [Online].Available at: https://www.techiedelight.com/declare-initialize-array-kotlin/ [Accessed: 22 March 2023].
+//Techie Delight .2016. Find minimum and maximum elements in an array in Kotlin, Techie Delights. [Online].Available at: https://www.techiedelight.com/find-minimum-maximum-element-array-kotlin/ [Accessed: 22 March 2023].
+//Techie Delight .2017. Find average of all items in a List in Kotlin, Techie Delights. [Online].Available at: https://www.techiedelight.com/find-average-of-all-items-in-a-list-in-kotlin/ [Accessed: 22 March 2023].
+//The IIE.2023. OPEN SOURCE CODING (INTRODUCTION)[OPSC6311 Module Manual].The Independent Institute of Education: Unpublished.
+//Schwarz, S. 2021. How to open a new activity with a button click -Android Kotlin Example, Medium. [Online]. Available at: https://medium.com/@simple.schwarz/how-to-open-a-new-activity-with-a-button-click-android-kotlin-example-bd2107946bbe [Accessed: 21 March 2023].
--- app/src/main/java/com/example/mycalculator/StatisticFunctions.kt
+++ app/src/main/java/com/example/mycalculator/StatisticFunctions.kt
@@ -1,11 +1,76 @@
 package com.example.mycalculator
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class StatisticFunctions : AppCompatActivity() {
+    //Initializing an ArrayList of Integers called arrayListOfNumbers with 10 elements, all initialized to 0(Techie Delight, 2017).
+    var arrayListOfNumbers = arrayListOf<Int>(0,0,0,0,0,0,0,0,0,0)
+
+    //Declaring variable and initializing it with the value 0.
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_statistic_function)
+
+        //Declaring variables(The IIE,2023)
+        var editTextStatisticNumber = findViewById<EditText>(R.id.editTextStatisticNumber)
+        var textViewDisplayStoredNumbers = findViewById<TextView>(R.id.textViewDisplayStoredNumbers)
+        var textViewDisplayAnswer = findViewById<TextView>(R.id.textViewDisplayAnswer)
+        var textViewDisplayError = findViewById<TextView>(R.id.textViewDisplayError)
+
+        //Declaring buttons(The IIE,2023)
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val btnClear = findViewById<Button>(R.id.btnClear)
+        val btnCalculateAverage = findViewById<Button>(R.id.btnCalculateAverage)
+        val btnFindMinAndMax = findViewById<Button>(R.id.btnFindMinAndMax)
+
+        //Add button(The IIE,2023)
+        btnAdd?.setOnClickListener {
+            Toast.makeText(this@StatisticFunctions, "Add button clicked", Toast.LENGTH_LONG).show()
+
+            arrayListOfNumbers[counter] = editTextStatisticNumber.text.toString().toInt()// The code adds a number to the arrayListOfNumbers array. The number is obtained from the EditText called editTextStatisticNumber, which is converted to an integer using the toInt() method.
+            counter++ // The position in the array where the number is added is determined by a counter variable called counter, which is incremented after each number is added.
+            textViewDisplayStoredNumbers.text = arrayListOfNumbers.joinToString(" ")//The joinToString() method is used to convert the array to a string with each element separated by a space.
+            editTextStatisticNumber.setText("")
+            if (counter == 10) {//If statement for when the user enters the 10th number to display that there can only be 10 numbers entered.
+                textViewDisplayError.text = "More than 10 numbers can't be entered!"//Output message
+                textViewDisplayError.setTextColor(Color.parseColor("red"))//Displaying error message in red so users can know not to enter more then two numbers
+            }
+        }
+        //Clear button(The IIE,2023)
+        btnClear?.setOnClickListener {
+            Toast.makeText(this@StatisticFunctions, "Clear button clicked", Toast.LENGTH_LONG).show()
+
+            //Resetting text views
+            textViewDisplayStoredNumbers.text=""
+            textViewDisplayError.text = ""
+            textViewDisplayAnswer.text = ""
+            //Resetting array list
+            arrayListOfNumbers=arrayListOf<Int>(0,0,0,0,0,0,0,0,0,0)
+            //Resetting counter variable
+            counter = 0
+        }
+        //Calculate Average button(The IIE,2023)
+        btnCalculateAverage?.setOnClickListener {
+            Toast.makeText(this@StatisticFunctions, "Calculate Average button clicked", Toast.LENGTH_LONG).show()
+
+            val avg = arrayListOfNumbers.average()//The average of the ArrayList of numbers is calculated using the average() function(Techie Delight, 2017).
+            textViewDisplayAnswer.text = "Average = ${avg}" //Displaying average
+        }
+        //Find Min and Max button(The IIE,2023)
+        btnFindMinAndMax?.setOnClickListener {
+            Toast.makeText(this@StatisticFunctions, "Find Min And Max button clicked", Toast.LENGTH_LONG).show()
+
+            var max = arrayListOfNumbers.indices.maxOfOrNull { i: Int -> arrayListOfNumbers[i] } //The maxOfOrNull() method is used to find the maximum value in the array list.The indices property is used to get the indices of the array list(Techie Delight, 2016).
+            var min = arrayListOfNumbers.indices.minOfOrNull { i: Int -> arrayListOfNumbers[i] } //The minOfOrNull() method is used to find the minimum value in the array list.The indices property is used to get the indices of the array list(Techie Delight, 2016).
+            textViewDisplayAnswer.text = ("MAX= ${max} MIN= ${min}") //Displaying maximum and minimum values
+
+        }
     }
 }
